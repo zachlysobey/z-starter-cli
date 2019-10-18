3612b17e3a47beef707c1cc02ff69b58fc23b6aa
Zach Lysobey <zlysobey@gmail.com>
2019-10-18 00:29:53 -0400

    ➕ add inquirer

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
 const chalk = require('chalk')
+const inquirer = require('inquirer')
 
-console.log('Hello,', chalk.green('World!'))
+const questions = [
+    // @see https://github.com/SBoudrias/Inquirer.js/tree/master/packages/inquirer/examples
+    {
+        type: 'input',
+        name: 'firstName',
+        message: "What's your first name",
+    },
+]
+
+inquirer.prompt(questions).then(function main(answers) {
+    console.log(`Hello, ${chalk.green(answers.firstName)}`)
+})
