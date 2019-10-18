a4270e95ca190313becfdf538cddffcd82562d22
Zach Lysobey <zlysobey@gmail.com>
2019-10-18 00:54:59 -0400

    🥩  beef up CLI

--- src/main.js
+++ src/main.js
@@ -1,15 +1,30 @@
 const chalk = require('chalk')
 const inquirer = require('inquirer')
+const figlet = require('figlet')
+
+const packageJson = require('../package.json')
 
 const questions = [
     // @see https://github.com/SBoudrias/Inquirer.js/tree/master/packages/inquirer/examples
     {
         type: 'input',
-        name: 'firstName',
-        message: "What's your first name",
+        name: 'name',
+        message: 'Who should I say hello to?',
+        default: 'World!',
     },
 ]
 
-inquirer.prompt(questions).then(function main(answers) {
-    console.log(`Hello, ${chalk.green(answers.firstName)}`)
-})
+;(async function main() {
+    try {
+        printAsciiArt(packageJson.name)
+        const answers = await inquirer.prompt(questions)
+        console.log(`Hello, ${chalk.green(answers.name)}`)
+    } catch (e) {
+        console.error('Fatal Error', e)
+        process.exit(1)
+    }
+})()
+
+function printAsciiArt(content) {
+    console.log(chalk.cyan(figlet.textSync(content)))
+}
